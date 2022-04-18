09be9bd783ebfa09420caf0a0779a0979be51793
YI JIN <kayjinyi@gmail.com>
2022-04-17 22:16:17 -0700

    updates

--- assets/script.js
+++ assets/script.js
@@ -12,61 +12,38 @@ var timeInterval = setInterval(function () {
   }, 1000);
 
   var questions = [
-    {q: "Which of the following type of variable is visible only within a function where it is defined?",
+    {q: "Commonly used data types DO NOT include__",
     ans: {
-        a: "1. global variable",
-        b: "2. local variable",
-        c: "3. Both of the above",
-        d: "4. None of the above",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
     },
-    answer: "2. local variable",
+    answer: "alerts",
     },
     {q: "Which of the following code creates an object?",
     ans: {
-        a: "1. var book = Object();",
-        b: "2. var book = new Object();",
-        c: "3. var book = new OBJECT();",
-        d: "4. var book = new Book();",
+        a: "var book = Object();",
+        b: "var book = new Object();",
+        c: "var book = new OBJECT();",
+        d: "var book = new Book();",
     },
-    answer: "2. var book = new Object();",
+    answer: "var book = new Object();",
     },
-    {q: "Which of the following function of String object returns the calling string value converted to upper case?",
+    {q: "The condition in an if /else statement is enclosed within__",
     ans: {
-        a: "1. toLocaleUpperCase()",
-        b: "2. toUpperCase()",
-        c: "3. toString()",
-        d: "4. substring()",
+        a: "quotes",
+        b: "curly brakets",
+        c: "parentheses",
+        d: "square braket",
     },
-    answer: "2. toUpperCase()",
+    answer: "parentheses",
     },
-    {q: "Which of the following function of Array object joins all elements of an array into a string?",
-    ans: {
-        a: "1. concat()",
-        b: "2. join()",
-        c: "3. pop()",
-        d: "4. map()",
-    },
-    answer: "2. join()",
-    },
-    {q: "Which of the following function of Array object returns a string representing the array and its elements?",
-    ans: {
-        a: "1. toSource()",
-        b: "2. sort()",
-        c: "3. splice()",
-        d: "4. toString()",
-    },
-    answer: "4. toString()",
-    }
 ];
-var qNum = 0
-
+var qNum = 0;
 console.log("linked");
 var startQuestion = document.querySelector("#startQ")
 console.log(startQuestion);
-// var Questions = {
-//     question:["Commonly used data types DO NOT include__", "The condition in an if /else statement is enclosed within__"],
-//     answers:[[["strings"],["booleans"],["alerts"],["numbers"]],[[["quotes"],["curly brakets"],["parentheses"],[square bre]]]
-// }
 startQuestion.addEventListener("click",function(event){
     event.preventDefault();
     //console.log(event);
